9e59553bc9ae5ba0331a11b516dbe4c1f3d69cf7
Naing Min <j6dr23n@gmail.com>
2023-03-20 04:00:45 +0630

    🖥️ wip:

--- .scripts/deploy.sh
+++ .scripts/deploy.sh
@@ -8,7 +8,7 @@ echo "Deployment started ..."
 (php artisan down) || true
 
 # Pull the latest version of the app
-git pull origin main
+git pull origin main --force
 
 # Install composer dependencies
 composer install --optimize-autoloader --no-dev --no-interaction
