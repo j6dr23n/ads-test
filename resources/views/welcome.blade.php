880f494e4b5512c69db01e2fe1478377eb9ac322
Naing Min <j6dr23n@gmail.com>
2023-03-20 18:37:18 +0630

    🖥️ wip:

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Testing 2</title>
+    <title>Laravel Testing 3</title>
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.bunny.net">
